96aaad7fa675a5342f190dafffae4c4a10713c30
rahul verma <rahul281191@gmail.com>
2025-06-07 10:55:06 +0530

    added mock data and user stories states in app file

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
+import { useState } from "react";
 import "./App.css";
+import StoryViewer from "./components/StoryViewer";
+import UsersStoryList from "./components/UsersStoryList";
+import type { UserStories } from "./types/story";
 
-function App() {
+const App: React.FC = () => {
+  const [usersStories, setUsersStories] = useState<UserStories[]>([]);
+  const [activeUserIndex, setActiveUserIndex] = useState<number | null>(null);
+
   return (
     <>
-      <h2>Hello</h2>
+      <div style={{ maxWidth: 480, margin: "0 auto" }}>
+        <UsersStoryList />
+        <StoryViewer />
+      </div>
     </>
   );
-}
+};
 
 export default App;
---
+++ src/data/mockData.ts
@@ -0,0 +1,130 @@
+import type { UserStories } from "../types/story";
+
+export const mockStories: UserStories[] = [
+  {
+    userId: 1,
+    username: "Rahul",
+    stories: [
+      {
+        id: 101,
+        imageUrl:
+          "https://images.pexels.com/photos/30921000/pexels-photo-30921000/free-photo-of-charming-backyard-patio-with-black-cat-and-chairs.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load",
+      },
+      {
+        id: 102,
+        imageUrl:
+          "https://images.pexels.com/photos/30768831/pexels-photo-30768831/free-photo-of-creative-floral-layout-with-polaroid-aesthetics.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load",
+      },
+      {
+        id: 103,
+        imageUrl:
+          "https://images.pexels.com/photos/32195327/pexels-photo-32195327/free-photo-of-intimate-moment-of-couple-holding-hands.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load",
+      },
+    ],
+  },
+  {
+    userId: 2,
+    username: "Kabir",
+    stories: [
+      {
+        id: 201,
+        imageUrl:
+          "https://images.pexels.com/photos/30488847/pexels-photo-30488847/free-photo-of-art-deco-cinema-facade-in-aguascalientes-mexico.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load",
+      },
+      {
+        id: 202,
+        imageUrl:
+          "https://images.pexels.com/photos/31280610/pexels-photo-31280610/free-photo-of-elegant-magnolia-blossoms-against-wall.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load",
+      },
+    ],
+  },
+  {
+    userId: 3,
+    username: "Arjun",
+    stories: [
+      {
+        id: 301,
+        imageUrl:
+          "https://images.pexels.com/photos/29213973/pexels-photo-29213973/free-photo-of-picturesque-village-with-foggy-morning-landscape.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load",
+      },
+      {
+        id: 302,
+        imageUrl:
+          "https://images.pexels.com/photos/32346392/pexels-photo-32346392/free-photo-of-woman-taking-photo-in-a-modern-elevator.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load",
+      },
+      {
+        id: 303,
+        imageUrl:
+          "https://images.pexels.com/photos/30563259/pexels-photo-30563259/free-photo-of-footprints-in-sand-beach-serenity.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load",
+      },
+      {
+        id: 304,
+        imageUrl:
+          "https://images.pexels.com/photos/32394181/pexels-photo-32394181/free-photo-of-young-couple-enjoying-outdoor-picnic-with-burgers.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load",
+      },
+    ],
+  },
+  {
+    userId: 4,
+    username: "Payal",
+    stories: [
+      {
+        id: 101,
+        imageUrl:
+          "https://images.pexels.com/photos/30921000/pexels-photo-30921000/free-photo-of-charming-backyard-patio-with-black-cat-and-chairs.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load",
+      },
+      {
+        id: 102,
+        imageUrl:
+          "https://images.pexels.com/photos/30768831/pexels-photo-30768831/free-photo-of-creative-floral-layout-with-polaroid-aesthetics.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load",
+      },
+      {
+        id: 103,
+        imageUrl:
+          "https://images.pexels.com/photos/32195327/pexels-photo-32195327/free-photo-of-intimate-moment-of-couple-holding-hands.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load",
+      },
+    ],
+  },
+  {
+    userId: 5,
+    username: "Roshit",
+    stories: [
+      {
+        id: 201,
+        imageUrl:
+          "https://images.pexels.com/photos/30488847/pexels-photo-30488847/free-photo-of-art-deco-cinema-facade-in-aguascalientes-mexico.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load",
+      },
+      {
+        id: 202,
+        imageUrl:
+          "https://images.pexels.com/photos/31280610/pexels-photo-31280610/free-photo-of-elegant-magnolia-blossoms-against-wall.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load",
+      },
+    ],
+  },
+  {
+    userId: 6,
+    username: "Mira",
+    stories: [
+      {
+        id: 301,
+        imageUrl:
+          "https://images.pexels.com/photos/29213973/pexels-photo-29213973/free-photo-of-picturesque-village-with-foggy-morning-landscape.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load",
+      },
+      {
+        id: 302,
+        imageUrl:
+          "https://images.pexels.com/photos/32346392/pexels-photo-32346392/free-photo-of-woman-taking-photo-in-a-modern-elevator.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load",
+      },
+      {
+        id: 303,
+        imageUrl:
+          "https://images.pexels.com/photos/30563259/pexels-photo-30563259/free-photo-of-footprints-in-sand-beach-serenity.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load",
+      },
+      {
+        id: 304,
+        imageUrl:
+          "https://images.pexels.com/photos/32394181/pexels-photo-32394181/free-photo-of-young-couple-enjoying-outdoor-picnic-with-burgers.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load",
+      },
+    ],
+  },
+];
